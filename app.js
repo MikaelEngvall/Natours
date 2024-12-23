e1d2fe6020d338d0bd4cc8f335985be2367b30f2
Mikael Engvall <mikael.engvall.me@gmail.com>
2024-12-23 04:07:47 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,168 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+const cookieParser = require('cookie-parser');
+const compression = require('compression');
+const cors = require('cors');
+
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
+const tourRouter = require('./routers/tourRoutes');
+const userRouter = require('./routers/userRoutes');
+const reviewRouter = require('./routers/reviewRoutes');
+const viewRouter = require('./routers/viewRoutes');
+const bookingRouter = require('./routers/bookingRoutes');
+const bookingController = require('./controllers/bookingController');
+
+// Start express app
+const app = express();
+
+app.enable('trust proxy');
+app.set('trust proxy', 1);
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
+// 1) GLOBAL MIDDLEWARES
+// Implement CORS
+app.use(cors());
+app.options('*', cors());
+// Access-Control-Allow-Origin *
+// api.natours.com
+// natour.com
+// app.use(cors({
+//   origin: 'https://www.natours.com'
+// }))
+
+// only put patch delete requests action here and none of other resources
+// app.options('/api/v1/tours/:id', cors());
+
+// Serving static files
+// app.use(express.static(`${__dirname}/public`));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Set Security HTTP headers
+const scriptSrcUrls = [
+  'https://unpkg.com/',
+  'https://tile.openstreetmap.org',
+  'https://cdnjs.cloudflare.com/ajax/libs/axios/1.3.2/axios.min.js',
+  'https://js.stripe.com/v3/'
+];
+const styleSrcUrls = [
+  'https://unpkg.com/',
+  'https://tile.openstreetmap.org',
+  'https://fonts.googleapis.com/',
+  'https://js.stripe.com/'
+];
+const connectSrcUrls = [
+  'https://unpkg.com',
+  'https://tile.openstreetmap.org',
+  'ws://localhost:*/',
+  'https://js.stripe.com/'
+];
+const fontSrcUrls = ['fonts.googleapis.com', 'fonts.gstatic.com'];
+
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: [],
+      connectSrc: [
+        "'self'",
+        'https:',
+        'http:',
+        'blob:',
+        'data:',
+        "'unsafe-inline'",
+        ...connectSrcUrls
+      ],
+      scriptSrc: ["'self'", ...scriptSrcUrls],
+      styleSrc: ["'self'", 'https:', "'unsafe-inline'", ...styleSrcUrls],
+      workerSrc: ["'self'", 'blob:', 'data:'],
+      objectSrc: ["'none'"],
+      childSrc: ["'self'", 'blob:', 'https://js.stripe.com/'],
+      imgSrc: ["'self'", 'blob:', 'data:', 'https:'],
+      fontSrc: ["'self'", ...fontSrcUrls],
+      formAction: ["'self'"],
+      upgradeInsecureRequests: []
+    }
+  })
+);
+
+// Development logging
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// Limit requests from same API
+// 100 request in one hour
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!'
+});
+
+app.use('/api', limiter);
+
+// stripe return as a stream not in json, req.body not in json
+app.post(
+  '/webhook-checkout',
+  express.raw({ type: 'application/json' }),
+  bookingController.webhookCheckout
+);
+
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+app.use(cookieParser());
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsAverage',
+      'ratingsQuantity',
+      'maxGroupSize',
+      'difficulty',
+      'price'
+    ]
+  })
+);
+
+app.use(compression());
+
+// Test middleware
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  // console.log(req.cookies);
+  next();
+});
+
+// 3) ROUTES
+app.use('/', viewRouter);
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/reviews', reviewRouter);
+app.use('/api/v1/bookings', bookingRouter);
+
+//*: stands for everything
+app.all('*', (req, res, next) => {
+  // Express automatically assumes that every argument passed in is an error
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 400));
+});
+
+app.use(globalErrorHandler);
+
+// 4) START THE SERVER
+module.exports = app;
